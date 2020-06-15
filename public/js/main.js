cad0376e7f8ede7455fbea93f89c5600acb7196c
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-16 01:20:44 +0800

    buttons work, just adding each projects information and styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,66 @@
 $(document).ready(function () {
+    
+//  function showSlides(){
+//         let slideIndex = 0;
+//         let i;
+//         const slides = $(".slides")
+//         for (i=0; i < slides.length; i++){
+//             slides[i].style.display = "none";
+//         }
+//         slideIndex++;
+//         if (slideIndex > slides.length){
+//             slideIndex = 1
+//         }
+//             slides[slideIndex-1].style.display = "grid";
+//             setTimeout(showSlides, 3000);
+//         }
+
+//         showSlides()
+
+    $(".prjBox").on("click", function(){
+        $(this).children(".wkTtl").toggleClass("hide");
+        $(this).children(".prjInfo").toggleClass("hide");
+    })
+
+    $("#rightBtn").on("click", function(){
+        let rightID = $(".imageShow")
+        let slideId = rightID.data("slide")
+        console.log(slideId)
+        console.log(slideId+1)
+        let nextId = slideId+1
+        if (slideId === 5) {
+            $("#slide5").toggleClass("imageShow");
+            $("#slide5").toggleClass("imageHide");
+            $("#slide1").toggleClass("imageHide");
+            $("#slide1").toggleClass("imageShow")
+        }else{
+            $("#slide"+ slideId).removeClass("imageShow");
+            $("#slide"+ slideId).addClass("imageHide");
+            $("#slide"+ nextId).removeClass("imageHide");
+            $("#slide"+ nextId).addClass("imageShow");
+        }
+    })
+
+    $("#leftBtn").on("click", function(){
+        let leftID = $(".imageShow")
+        let slideId = leftID.data("slide")
+        console.log(slideId)
+        console.log(slideId-1)
+        let nextId = slideId-1
+        if (slideId === 1) {
+            $("#slide1").toggleClass("imageShow");
+            $("#slide1").toggleClass("imageHide");
+            $("#slide5").toggleClass("imageHide");
+            $("#slide5").toggleClass("imageShow")
+        }else{
+            $("#slide"+ slideId).removeClass("imageShow");
+            $("#slide"+ slideId).addClass("imageHide");
+            $("#slide"+ nextId).removeClass("imageHide");
+            $("#slide"+ nextId).addClass("imageShow");
+        }
+        })
+    
+
 
     $("#contact").click(function () {
         $("#disclaimer").toggleClass("active");
@@ -16,6 +78,16 @@ $(document).ready(function () {
             $("#section-container").toggleClass("hide");
             $("footer").toggleClass("hide");
     })
+
+
+    // $("#project").click(function(){
+    //     $("#section-container").toggleClass("hide");
+    //     $("footer").toggleClass("hide");
+    // })
+
+    // $(window).on("scroll touchmove", function(){
+    //     $("#navbar").toggleClass("tiny", "h-screen", $(document).scrollTop() > 0)
+    // })
 
 })
 
