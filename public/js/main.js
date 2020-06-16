a324a0e19c6fe87657b18c9edc32ef5ae02b7a96
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-17 00:19:44 +0800

    all buttons added to main

--- public/js/main.js
+++ public/js/main.js
@@ -1,100 +1,96 @@
 $(document).ready(function () {
-    
-//  function showSlides(){
-//         let slideIndex = 0;
-//         let i;
-//         const slides = $(".slides")
-//         for (i=0; i < slides.length; i++){
-//             slides[i].style.display = "none";
-//         }
-//         slideIndex++;
-//         if (slideIndex > slides.length){
-//             slideIndex = 1
-//         }
-//             slides[slideIndex-1].style.display = "grid";
-//             setTimeout(showSlides, 3000);
-//         }
 
-//         showSlides()
+  $(".closeBtn").click(function(e){
+    event.stopPropagation()
+    let weekID = $("#"+$(this).data("week")) 
+    weekID.children(".closeBtn").toggleClass("hide")
+    weekID.children(".wkTtl").toggleClass("hide")
+    weekID.children(".prjInfo").toggleClass("hide")
+  })
 
-    $(".prjBox").on("click", function(){
-        $(".wkTtl").removeClass("hide");
-        $(".prjInfo").addClass("hide");
-        $(this).children(".wkTtl").toggleClass("hide");
-        $(this).children(".prjInfo").toggleClass("hide");
-    })
+  $("#contact").click(function () {
+    $("#disclaimer").toggleClass("active");
+    $("#overlay").toggleClass("active");
+  });
 
-    $("#rightBtn").on("click", function(){
-        let rightID = $(".imageShow")
-        let slideId = rightID.data("slide")
-        console.log(slideId)
-        console.log(slideId+1)
-        let nextId = slideId+1
-        if (slideId === 5) {
-            $("#slide5").toggleClass("imageShow");
-            $("#slide5").toggleClass("imageHide");
-            $("#slide1").toggleClass("imageHide");
-            $("#slide1").toggleClass("imageShow")
-        }else{
-            $("#slide"+ slideId).removeClass("imageShow");
-            $("#slide"+ slideId).addClass("imageHide");
-            $("#slide"+ nextId).removeClass("imageHide");
-            $("#slide"+ nextId).addClass("imageShow");
-        }
-    })
+  $("#project").click(function () {
+    $("#navbar").toggleClass("hide");
+    $("#buttonSeq").toggleClass("preNavHeader");
+    $("#buttonSeq").toggleClass("navHeader");
+    $("#project").children().toggleClass("hide");
+    $("#section-container").toggleClass("hide");
+    $("footer").toggleClass("hide");
+  });
 
-    $("#leftBtn").on("click", function(){
-        let leftID = $(".imageShow")
-        let slideId = leftID.data("slide")
-        console.log(slideId)
-        console.log(slideId-1)
-        let nextId = slideId-1
-        if (slideId === 1) {
-            $("#slide1").toggleClass("imageShow");
-            $("#slide1").toggleClass("imageHide");
-            $("#slide5").toggleClass("imageHide");
-            $("#slide5").toggleClass("imageShow")
-        }else{
-            $("#slide"+ slideId).removeClass("imageShow");
-            $("#slide"+ slideId).addClass("imageHide");
-            $("#slide"+ nextId).removeClass("imageHide");
-            $("#slide"+ nextId).addClass("imageShow");
-        }
-        })
-    
+  $(".prjBox").on("click", function () {
+    $(".wkTtl").removeClass("hide");
+    $(".prjInfo").addClass("hide");
+    $(".closeBtn").addClass("hide");
+    $(this).children(".wkTtl").toggleClass("hide");
+    $(this).children(".prjInfo").toggleClass("hide");
+    $(this).children(".closeBtn").removeClass("hide");
+  });
 
+  $("#rightBtn").on("click", function () {
+    let rightID = $(".imageShow");
+    let slideId = rightID.data("slide");
+    let nextId = slideId + 1;
+    if (slideId === 5) {
+      $("#slide5").toggleClass("imageShow");
+      $("#slide5").toggleClass("imageHide");
+      $("#slide1").toggleClass("imageHide");
+      $("#slide1").toggleClass("imageShow");
+    } else {
+      $("#slide" + slideId).removeClass("imageShow");
+      $("#slide" + slideId).addClass("imageHide");
+      $("#slide" + nextId).removeClass("imageHide");
+      $("#slide" + nextId).addClass("imageShow");
+    }
+  });
 
-    $("#contact").click(function () {
-        $("#disclaimer").toggleClass("active");
-        $("#overlay").toggleClass("active");
-    })
+  $("#leftBtn").on("click", function () {
+    let leftID = $(".imageShow");
+    let slideId = leftID.data("slide");
+    let nextId = slideId - 1;
+    if (slideId === 1) {
+      $("#slide1").toggleClass("imageShow");
+      $("#slide1").toggleClass("imageHide");
+      $("#slide5").toggleClass("imageHide");
+      $("#slide5").toggleClass("imageShow");
+    } else {
+      $("#slide" + slideId).removeClass("imageShow");
+      $("#slide" + slideId).addClass("imageHide");
+      $("#slide" + nextId).removeClass("imageHide");
+      $("#slide" + nextId).addClass("imageShow");
+    }
+  });
 
-    $("#project").click(function () {
-       
-            $("#navbar").toggleClass("hide");
-            $("#buttonSeq").toggleClass("preNavHeader");
-            $("#buttonSeq").toggleClass("navHeader");
-            $("#house").toggleClass("hide");
-            $("#smoke").toggleClass("hide");
-            $("button").children().toggleClass("hide");
-            $("#section-container").toggleClass("hide");
-            $("footer").toggleClass("hide");
-    })
+  //  function showSlides(){
+  //         let slideIndex = 0;
+  //         let i;
+  //         const slides = $(".slides")
+  //         for (i=0; i < slides.length; i++){
+  //             slides[i].style.display = "none";
+  //         }
+  //         slideIndex++;
+  //         if (slideIndex > slides.length){
+  //             slideIndex = 1
+  //         }
+  //             slides[slideIndex-1].style.display = "grid";
+  //             setTimeout(showSlides, 3000);
+  //         }
 
+  //         showSlides()
 
-    // $("#project").click(function(){
-    //     $("#section-container").toggleClass("hide");
-    //     $("footer").toggleClass("hide");
-    // })
+  // $("#project").click(function(){
+  //     $("#section-container").toggleClass("hide");
+  //     $("footer").toggleClass("hide");
+  // })
 
-    // $(window).on("scroll touchmove", function(){
-    //     $("#navbar").toggleClass("tiny", "h-screen", $(document).scrollTop() > 0)
-    // })
-
-})
-
-
-
+  // $(window).on("scroll touchmove", function(){
+  //     $("#navbar").toggleClass("tiny", "h-screen", $(document).scrollTop() > 0)
+  // })
+});
 
 // $("#about").click(function(){
 //   $("#prjSection").addClass("hide");
