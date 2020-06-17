1a249293d3659bc7463945303de2f26ab2380652
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-17 23:57:32 +0800

    removed all files related to passport

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,3 @@
-const path = require("path");
-const isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function (app) {
   // get request to index.handlebars
   app.get("/", (req, res) => {
@@ -13,25 +10,5 @@ module.exports = function (app) {
     }
     res.render("contact")
   })
-
-  app.get("/signup", function(req, res) {
-
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/mock/signup.html"));
-  });
-
-  app.get("/login", function(req, res) {
-  
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/mock/login.html"));
-  });
-
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/mock/members.html"));
-  });
 
 };
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 // NPM Packages
 const chalk = require("chalk")
 const express = require("express");
-const session = require("express-session");
 const exphbs = require("express-handlebars");
 const app = express();
-const passport = require("./config/passport");
 
 // Port
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
 
 // Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -19,28 +16,20 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Passport
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
 //Requiring our routes
 require("./routes/html-routes.js")(app)
-require("./routes/api-routes.js")(app)
+
 
 //sequelize and listener
-db.sequelize.sync({force:false}).then(function() {
-    app.listen(PORT, function () {
-        console.log(
-          chalk.bold.blue("Listening on port %s. 🌎"),
-          chalk.bold.yellow(PORT) +
-            chalk.bold.green("\nVisit ") +
-            chalk.bold.blue("http://localhost:") +
-            chalk.bold.yellow(PORT) +
-            chalk.bold.blue("/") +
-            chalk.bold.green(" in your browser")
-        );
-      });
- });
+app.listen(PORT, function () {
+    console.log(
+      chalk.bold.blue("Listening on port %s. 🌎"),
+      chalk.bold.yellow(PORT) +
+        chalk.bold.green("\nVisit ") +
+        chalk.bold.blue("http://localhost:") +
+        chalk.bold.yellow(PORT) +
+        chalk.bold.blue("/") +
+        chalk.bold.green(" in your browser")
+    );
+  });
 
